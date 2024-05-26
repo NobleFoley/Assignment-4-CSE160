1de372ed5648f31d69545fa928b3ece4ace93e9c
Noble Foley <noafoley@ucsc.edu>
2024-05-25 17:00:00 -0700

    no json

--- src/World.js
+++ src/World.js
@@ -1306,8 +1306,8 @@ function deleteBlock() {
   if (x > 0 && x < 32 && y > 0 && y < 32) {
     if (g_map[heigh][x][y] == 1) {
       g_map[heigh][x][y] = 0;
-      var data = JSON.stringify(g_map);
-      console.log(data);
+      //var data = JSON.stringify(g_map);
+      //console.log(data);
       return;
     }
     console.log("block deleted at", x, y);
